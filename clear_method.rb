5ca98d6235de552eec96a7e8e835b2168b8c3cd2
JBrow95 <browningjustin95@gmail.com>
2018-06-28 14:43:13 -0400

    test failed, length is 1

--- clear_method.rb
+++ clear_method.rb
@@ -1,3 +1,3 @@
 def rm_elements()
-	f_array = [1,2,3,4,5]
+	f_array = [1]
 end
--- tdd_clear.rb
+++ tdd_clear.rb
@@ -10,4 +10,8 @@ class TestClearMethod < Minitest::Test
 	def test_if_array_has_elements
 		assert_equal(1, rm_elements()[0])
 	end
+
+	def test_if_array_length_is_5
+		assert_equal(5, rm_elements().length)
+	end
 end
