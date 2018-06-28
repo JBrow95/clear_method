f52b815b4a97119da4ae0347a22b3ac492c341a8
JBrow95 <browningjustin95@gmail.com>
2018-06-28 14:36:04 -0400

    test failed, array class nil

--- tdd_clear.rb
+++ tdd_clear.rb
@@ -1,9 +1,9 @@
 require "minitest/autorun"
-require_relative "upcase_method.rb"
+require_relative "clear_method.rb"
 
-class TestUpcaseMethod < Minitest::Test
+class TestClearMethod < Minitest::Test
 
-	
-
-
+	def test_if_array_is_class
+		assert_equal(Array, rm_elements().class)
+	end
 end
