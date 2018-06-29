dd6760f8a20bca3799f613d66965314a24d12bb5
JBrow95 <browningjustin95@gmail.com>
2018-06-29 15:21:36 -0400

    rewrote func with a .drop method that takes every element in the array and deletes it, made test to see if array is clear using strings and it passed

--- clear_method.rb
+++ clear_method.rb
@@ -1,4 +1,5 @@
 f_array = []
 def rm_elements(f_array)
-	f_array.drop(5)
+	# f_array.drop(5)
+	f_array.drop_while {|n| n = f_array}
 end
--- tdd_clear.rb
+++ tdd_clear.rb
@@ -5,17 +5,8 @@ class TestClearMethod < Minitest::Test
 
 	def test_if_array_is_class
 		f_array = []
-		assert_equal(Array, rm_elements([1,2,3,4,5]).class)
+		assert_equal(Array, rm_elements([]).class)
 	end
-
-	# def test_if_array_has_elements
-	# 	f_array = []
-	# 	assert_equal(1, rm_elements([1,2,3,4,5])[0])
-	# 	assert_equal(2, rm_elements([1,2,3,4,5])[1])
-	# 	assert_equal(3, rm_elements([1,2,3,4,5])[2])
-	# 	assert_equal(4, rm_elements([1,2,3,4,5])[3])
-	# 	assert_equal(5, rm_elements([1,2,3,4,5])[4])
-	# end
 
 	# def test_if_pos_1_is_removed
 	# 	f_array = []
@@ -27,8 +18,14 @@ class TestClearMethod < Minitest::Test
 	# 	assert_equal([3,4,5], rm_elements([1,2,3,4,5]))
 	# end
 
-	def test_if_all_positions_are_removed
+	def test_if_all_positions_are_removed_with_integer
 		f_array = []
 		assert_equal([], rm_elements([1,2,3,4,5]))
 	end
+
+	def test_if_all_positions_are_removed_with_String
+		f_array = []
+		assert_equal([], rm_elements(["1","2","3","4"]))
+	end
+
 end
